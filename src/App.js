cf6003aed3821121c43ab931c1120e99620a9192
j-hutchison <jhutchison@outlook.de>
2022-09-09 14:50:54 +0200

    Initial working solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,128 @@
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
+import Canvas from "./components/canvas";
 
 function App() {
+	const ANSWER_DECK = [
+		"COLOMBIA",
+		"JAMAICA",
+		"SWITZERLAND",
+		"AUSTRALIA",
+		"ARGENTINA",
+		"MEXICO",
+		"MOZAMBIQUE",
+	];
+
+	/* Global event listeners are created at initial render and do not get updates to state on rerender, refs can be used to store updates and give access to event listeners */
+	const [answer, _setAnswer] = useState({
+		word: "",
+		letterArray: [{ letter: "", show: false }],
+	});
+	const answerRef = useRef(answer);
+	const setAnswer = (data) => {
+		answerRef.current = data;
+		_setAnswer(() => data);
+	};
+
+	const [wrongGuesses, _setWrongGuesses] = useState([]);
+	const wrongGuessesRef = useRef(wrongGuesses);
+
+	const setWrongGuesses = (data) => {
+		const updatedArray = [...wrongGuessesRef.current, data];
+		wrongGuessesRef.current = updatedArray;
+		_setWrongGuesses(updatedArray);
+	};
+
+	const [correctGuesses, _setCorrectGuesses] = useState([]);
+	const correctGuessesRef = useRef(correctGuesses);
+
+	const setCorrectGuesses = (data) => {
+		const updatedArray = [...correctGuessesRef.current, data];
+		correctGuessesRef.current = updatedArray;
+		_setCorrectGuesses(updatedArray);
+	};
+
+	//TODO - UPDATE TO STILL RUN ON INITIAL RENDER ONLY BUT ALSO W NECESSARY DEPENDENCIES
+	useEffect(() => {
+		const randomAnswer =
+			ANSWER_DECK[Math.floor(Math.random() * ANSWER_DECK.length)];
+
+		const letterArray = randomAnswer.split("").map((letter) => ({
+			letter: letter,
+			show: false,
+		}));
+		setAnswer({ word: randomAnswer, letterArray: letterArray });
+
+		window.addEventListener("keydown", handleKeyboardInput);
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyboardInput);
+		};
+	}, []);
+
+	const handleKeyboardInput = (e) => {
+		console.log(e.key);
+		const input = e.key;
+
+		if (
+			!answerRef.current.word.toLowerCase().includes(input) &&
+			!wrongGuessesRef.current.includes(input)
+		) {
+			setWrongGuesses(input);
+			console.log(`${answerRef.current.word} does not contain ${input}`);
+		}
+		if (
+			answerRef.current.word.toLowerCase().includes(input) &&
+			!correctGuessesRef.current.includes(input)
+		) {
+			setCorrectGuesses(input);
+
+			const updatedResults = answerRef.current.letterArray.map((el) => {
+				if (input.toLowerCase() === el.letter.toLowerCase()) {
+					return { ...el, show: true };
+				}
+				return { ...el };
+			});
+
+			setAnswer({ word: answerRef.current.word, letterArray: updatedResults });
+
+			console.log(`${answerRef.current.word} contains ${input}`);
+		}
+
+		console.log(`updated wrong array: ${wrongGuessesRef.current}`);
+		console.log(`updated correct array: ${correctGuessesRef.current}`);
+	};
+
 	return (
 		<div className="app">
-			<h1>Hangman!</h1>
+			<header>
+				<h1 className="primary-heading">Hangman</h1>
+				<p className="primary-description">
+					Find the hidden word - Enter a letter
+				</p>
+			</header>
+			<main className="game">
+				<div className="game-presentation">
+					<Canvas wrongGuesses={wrongGuessesRef.current} />
+					<aside className="game-wrong-letters">
+						<h2>Wrong Letters</h2>
+						<span>
+							{wrongGuessesRef.current.map((letter) => {
+								return <span>{letter}</span>;
+							})}
+						</span>
+					</aside>
+				</div>
+				<div className="game-answer">
+					{answer.letterArray.map((el) => {
+						return el.show ? (
+							<span className="game-answer-letter">{el.letter}</span>
+						) : (
+							<span className="game-answer-letter hidden"></span>
+						);
+					})}
+				</div>
+			</main>
 		</div>
 	);
 }
---
+++ src/components/canvas.js
@@ -0,0 +1,144 @@
+import React, { useRef, useEffect, useCallback, useMemo } from "react";
+import styles from "./canvas.module.css";
+
+const Canvas = ({ wrongGuesses }) => {
+	const canvasRef = useRef(null);
+
+	const frame = useMemo(() => {
+		return {
+			topLeft: [60, 60],
+			bottomCenter: [60, 300],
+			bottomLeft: [15, 300],
+			bottomRight: [105, 300],
+			topRight: [180, 60],
+			ropeEnd: [180, 90],
+		};
+	}, []);
+
+	const LINE_WIDTH = 5;
+	const shoulderPosition = 159;
+	const hipPosition = 215;
+	const armLength = 45;
+	const armElevation = 18;
+	const headRadius = 24;
+	const headStart = frame.ropeEnd[1] + headRadius;
+	const headEnd = headStart + headRadius;
+	const bodyLength = 78;
+	const bodyEndPoint = headEnd + bodyLength;
+	const legLength = 36;
+	const legAngle = 30;
+
+	const drawFrame = useCallback(
+		(ctx) => {
+			ctx.beginPath();
+			ctx.moveTo(frame.topLeft[0], frame.topLeft[1]);
+			ctx.lineTo(frame.bottomCenter[0], frame.bottomCenter[1]);
+			ctx.moveTo(frame.bottomLeft[0], frame.bottomLeft[1]);
+			ctx.lineTo(frame.bottomRight[0], frame.bottomRight[1]);
+			ctx.moveTo(frame.topLeft[0], frame.topLeft[1]);
+			ctx.lineTo(frame.topRight[0], frame.topRight[1]);
+			ctx.lineTo(frame.ropeEnd[0], frame.ropeEnd[1]);
+			ctx.stroke();
+		},
+		[frame]
+	);
+
+	/* DRAW FUNCTIONS */
+	const drawHead = (ctx) => {
+		ctx.beginPath();
+		ctx.arc(frame.ropeEnd[0], headStart, headRadius, 0, 2 * Math.PI);
+		ctx.stroke();
+	};
+	const drawBody = (ctx) => {
+		ctx.beginPath();
+		ctx.moveTo(frame.ropeEnd[0], headEnd);
+		ctx.lineTo(frame.ropeEnd[0], bodyEndPoint);
+
+		ctx.stroke();
+	};
+	const drawLeftArm = (ctx) => {
+		ctx.beginPath();
+		ctx.moveTo(frame.ropeEnd[0], shoulderPosition);
+		ctx.lineTo(frame.ropeEnd[0] - armLength, shoulderPosition - armElevation);
+
+		ctx.stroke();
+	};
+	const drawRightArm = (ctx) => {
+		ctx.beginPath();
+		ctx.moveTo(frame.ropeEnd[0], shoulderPosition);
+		ctx.lineTo(frame.ropeEnd[0] + armLength, shoulderPosition - armElevation);
+
+		ctx.stroke();
+	};
+	const drawLeftLeg = (ctx) => {
+		ctx.beginPath();
+		ctx.moveTo(frame.ropeEnd[0], hipPosition);
+		ctx.lineTo(frame.ropeEnd[0] - legAngle, bodyEndPoint + legLength);
+
+		ctx.stroke();
+	};
+	const drawRightLeg = (ctx) => {
+		ctx.beginPath();
+		ctx.moveTo(frame.ropeEnd[0], hipPosition);
+		ctx.lineTo(frame.ropeEnd[0] + legAngle, bodyEndPoint + legLength);
+
+		ctx.stroke();
+	};
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const context = canvas.getContext("2d");
+		context.strokeStyle = "#f9f9f9";
+		context.lineWidth = LINE_WIDTH;
+
+		drawFrame(context);
+
+		// drawHead(context);
+		// drawBody(context);
+		// drawLeftArm(context);
+		// drawRightArm(context);
+		// drawLeftLeg(context);
+		// drawRightLeg(context);
+	}, [drawFrame]);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const context = canvas.getContext("2d");
+		console.log(`# of incorrect guesses = ${wrongGuesses.length}`);
+
+		switch (wrongGuesses.length) {
+			case 1:
+				drawHead(context);
+				break;
+			case 2:
+				drawBody(context);
+				break;
+			case 3:
+				drawLeftArm(context);
+				break;
+			case 4:
+				drawRightArm(context);
+				break;
+			case 5:
+				drawLeftLeg(context);
+				break;
+			case 6:
+				drawRightLeg(context);
+				console.log(`You Lose!`);
+				break;
+			default:
+				console.log(`Let's Go!`);
+		}
+	}, [wrongGuesses]);
+
+	return (
+		<canvas
+			width="300px"
+			height="400px"
+			ref={canvasRef}
+			className={styles["game-canvas"]}
+		/>
+	);
+};
+
+export default Canvas;
